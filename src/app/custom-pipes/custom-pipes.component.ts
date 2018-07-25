318aa5acb70c1daac5d81603e599903309b4c8a6
DESKTOP-OKVKOOA\cmina <mina@wannabit.io>
2018-07-25 11:17:07 +0900

    custom pipe end

--- src/app/custom-pipes/custom-pipes.component.ts
+++ src/app/custom-pipes/custom-pipes.component.ts
@@ -5,6 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './custom-pipes.component.html',
   styleUrls: ['./custom-pipes.component.css']
 })
+
+// 앵귤러에서 파이프를 만드는 것은 간단하다.
+// Class에 @Pipe 데코레이터를 붙인다음 파라미터로 name으로 세팅하고, 
+// transform function만 구현하면 된다.
 export class CustomPipesComponent implements OnInit {
   imageUrl: string = "";
   
